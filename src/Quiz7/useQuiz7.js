be308f42ce034868af9ebb8a2ef2c67efe064097
mi-k-7 <rudd12@bu.ac.kr>
2024-06-13 14:53:43 +0900

    파일명 수정

--- src/Quiz7/useQuiz7.js
+++ src/Quiz7/useQuiz7.js
@@ -6,11 +6,12 @@ function useQuiz7(componentType) {
 
     var componentType;
     const answerNum = ['(가)', '(나)', '(다)', '(라)'];
-    if (componentType === 'A1_18_Quiz_1') {
+    // const answerCheck = ['나비 자세', '비둘기 자세', '굴렁쇠 자세', '아치 자세'];
+    if (componentType === 'Quiz7_1') {
         var answerCheck = ['나비 자세', '비둘기 자세', '굴렁쇠 자세', '아치 자세'];
-    } else if (componentType === 'A1_18_Quiz_2') {
+    } else if (componentType === 'Quiz7_2') {
         var answerCheck = ['앉은 자세', '앉은 자세', '누운 자세', '누운 자세'];
-    } else if (componentType === 'A1_18_Quiz_3') {
+    } else if (componentType === 'Quiz7_3') {
         var answerCheck = ['골반교정', '어깨결림 완화', '혈액순환', '허리통증 완화'];
     }
     
